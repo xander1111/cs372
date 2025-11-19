86a96840fbe6cbce38c8bfd8a93f12b22d6d8348
Xander Bailey <xander.bailey@gmail.com>
2025-11-19 08:44:24 -0800

    Complete project 8.1

--- project8.1/select_server.py
+++ project8.1/select_server.py
@@ -7,8 +7,34 @@ import socket
 import select
 
 def run_server(port):
-    # TODO--fill this in
-    pass
+    listener = socket.socket()
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    listener.bind(("localhost", port))
+    
+    listener.listen()
+    
+    sockets = set()
+    sockets.add(listener)
+    
+    while True:
+        ready, _, _ = select.select(sockets, {}, {})
+        
+        for s in ready:
+            if s is listener:
+                client_socket, client_addr = s.accept()
+                sockets.add(client_socket)
+                print(f"{client_addr}: connected")
+            else:
+                data = s.recv(1024)
+                
+                if len(data) == 0:
+                    sockets.remove(s)
+                    print(f"{s.getpeername()}: disconnected")
+                    
+                else:
+                    print(f"{s.getpeername()} {len(data)} bytes: b'{data.decode()}'")
+
 
 #--------------------------------#
 # Do not modify below this line! #
